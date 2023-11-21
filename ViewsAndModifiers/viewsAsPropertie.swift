9cbda8bc5820789c5984f03beabff695d1903dca
n0m4da <nomad._@hotmail.com>
2023-11-21 14:21:14 -0700

    viewsAsProperties

--- ViewsAndModifiers/viewsAsPropertie.swift
+++ ViewsAndModifiers/viewsAsPropertie.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct viewsAsPropertie: View {
+//    let motto = Text("Draco dormiens")
+//    let motto2 = Text("nunquam titillandus")
+    
+    var  motto : some View{
+        Text("Draco dormiens")
+    }
+    let motto2 = Text("nunquam titillandus")
+    
+    var spells: some View{
+        VStack{
+            Text("Lumos")
+            Text("Obliviate")
+            // the text must be be inside of stack otherwise it will return an error but not if we add @Viewbuilder  like in the example
+        }
+    }
+    
+    @ViewBuilder var spells2: some View{
+            Text("Imperius")
+            .foregroundStyle(.yellow)
+            Text("Crucio")
+            .foregroundStyle(.red)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10){
+            motto
+                .foregroundStyle(.red)
+            motto2
+                .foregroundStyle(.blue)
+            
+            spells
+            
+            spells2
+        }
+        .padding()
     }
 }
 
