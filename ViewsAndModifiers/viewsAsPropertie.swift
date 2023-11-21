6b89502216c6d05195ea0bfd693a7cc11ee91e22
n0m4da <nomad._@hotmail.com>
2023-11-21 14:20:52 -0700

    viewsAsProperties

---
+++ ViewsAndModifiers/viewsAsPropertie.swift
@@ -0,0 +1,18 @@
+//
+//  viewsAsPropertie.swift
+//  ViewsAndModifiers
+//
+//  Created by Luis Rivera on 21/11/23.
+//
+
+import SwiftUI
+
+struct viewsAsPropertie: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    viewsAsPropertie()
+}
