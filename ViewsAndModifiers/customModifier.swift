942d282a4733becb385760369114aef597c81ada
n0m4da <nomad._@hotmail.com>
2023-11-21 14:49:28 -0700

    modififer

---
+++ ViewsAndModifiers/customModifier.swift
@@ -0,0 +1,18 @@
+//
+//  customModifier.swift
+//  ViewsAndModifiers
+//
+//  Created by Luis Rivera on 21/11/23.
+//
+
+import SwiftUI
+
+struct customModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    customModifier()
+}
