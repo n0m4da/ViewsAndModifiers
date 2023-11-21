0c478ef403086efd7f3a0b1ded3a11974432d5f4
n0m4da <nomad._@hotmail.com>
2023-11-21 13:51:34 -0700

    finished

--- ViewsAndModifiers/enviromentModifiers.swift
+++ ViewsAndModifiers/enviromentModifiers.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct enviromentModifiers: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Grifindor")
+                .blur(radius: 0)// cant override outside blur modifier
+                .font(.largeTitle)//this override all modifier of vstack
+            Text("Hufflepuff")
+            Text("Ravenclaw")
+            Text("Slytherin")
+        }
+        .font(.title)// enviroment mofifier can be override
+        .blur(radius: 5)// regular modifier can't be override by inside modifier
     }
 }
 
