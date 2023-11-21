d678d05300a1b7e7ccd27ca4093ae6b7b59d860f
n0m4da <nomad._@hotmail.com>
2023-11-21 13:46:44 -0700

    enviroment modifier

---
+++ ViewsAndModifiers/enviromentModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  enviromentModifiers.swift
+//  ViewsAndModifiers
+//
+//  Created by Luis Rivera on 21/11/23.
+//
+
+import SwiftUI
+
+struct enviromentModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    enviromentModifiers()
+}
